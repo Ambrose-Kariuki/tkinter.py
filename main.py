2722526658ff7947a40b4693d2ad3ba1725b914b
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-03-28 10:53:16 +0300

    added title

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-
+root.title("Simple Tkinter App")
+root.geometry("300x200")
 root.mainloop()
 # This is a simple Tkinter application that creates a window with a title and a button.
