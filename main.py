f72fb30533fa6d72ac91430161d663908a5205c2
Ambrose-Kariuki <kariukiambrose093@gmail.com>
2025-03-28 11:19:12 +0300

    added label

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Simple Tkinter App")
-root.geometry("300x200")
+root.geometry("500x500")
+lable = tk.Label(root,text = "Hello world!",font=('arial', 18, 'bold'))
+lable.pack(padx = 10, pady = 10)
+text = tk.Text(root,height = 3, font=('arial', 16))
+text.pack()
+
 root.mainloop()
 # This is a simple Tkinter application that creates a window with a title and a button.
